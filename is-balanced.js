9e051bb4ad42ba7ab63061b268f9749d881ea146
Eric Sorum <ejsorum@gmail.com>
2025-04-29 07:17:26 -0500

    Balanced binary

--- is-balanced.js
+++ is-balanced.js
@@ -1,5 +1,19 @@
-// Given a binary tree, determine if it is height-balanced.
+/* 
+Given a binary tree, determine if it is height-balanced.
+A height-balanced binary tree is a binary tree in which the 
+depth of the two subtrees of every node never differs by more than one
+*/
 
 var isBalanced = function(root) {
+  // Loop through, check if null
+  // if null, see how many since the beginning.
+
+  // no... need to multiply index by 2...
+  // and then check if the value is shorter than the array length.
+  // if isn't, then there's gaps in the tree.
+  // 
+  for (let i = 1; i < root.length; i*2) {
     
+  }
+
 };
