4de993f051f65bd627e1c82b088fba4e2306d7e7
Eric Sorum <ejsorum@gmail.com>
2025-04-30 11:03:53 -0500

    Best time to buy

--- best-time-to-buy.js
+++ best-time-to-buy.js
@@ -1,3 +1,13 @@
 var maxProfit = function(prices) {
-    
+  let bestProfit = 0;
+  function checkForProfit(i) {
+    for (let j = i; j < prices.length; j++) {
+      bestProfit = prices[j] > prices [i] ? prices[j] - prices[i] : bestProfit;
+    }
+  }
+  for (let i = 0; i < prices.length; i++) {
+    checkForProfit(i);
+
+  }
+  return bestProfit;
 };
