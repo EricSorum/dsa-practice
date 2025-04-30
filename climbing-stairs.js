a015ff9bf3d6348fd2ee6106e80a37b1a4359088
Eric Sorum <ejsorum@gmail.com>
2025-04-30 10:55:45 -0500

    A few more starts.

--- climbing-stairs.js
+++ climbing-stairs.js
@@ -1,3 +1,22 @@
 var climbStairs = function(n) {
-    
+    // we divide the number by two, and it should just be the amount
+
+
+
+    /* let's try 7 steps
+    1,1,1,1,1,1,1 x 1
+    2,1,1,1,1,1 x 6
+    2,2,1,1,1 x 9
+    2,2,2,1 x 4
+
+    4 steps
+    1,1,1,1, x 1
+    2,1,1 x 3
+    2,2 x 1
+
+    5 steps
+    1,1,1,1,1 x 1
+    2,1,1,1 x 4
+    2,2,1 x 3
+    */
 };
---
+++ pascals-triangle.js
@@ -0,0 +1,17 @@
+var generate = function(numRows) {
+  /*
+  So each number is like
+  take the subarray before this subarray
+  and take the item in that array that's one behind the current index
+  and add it to the item after it
+
+    
+  */
+
+  // but we're creating the array as we go.
+  let triangle = [];
+  // let counter = 1;
+  for (let i = 0; i < numRows; i++) {
+    
+  }
+};
