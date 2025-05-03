5b2911be62502b5351824b55286df0227d334cee
Eric Sorum <ejsorum@gmail.com>
2025-05-03 10:25:58 -0500

    Isomorphic strings

--- isomorphic-strings.js
+++ isomorphic-strings.js
@@ -1,17 +1,5 @@
 var isIsomorphic = function(s, t) {
-  // let bool = false;
-  // add both letters together as an element in array
-  let newArr = [];
-  for (let i = 0; i < s.length; i++) {
-    const twoLetters = `${s[i]} ${t[i]}`;
-    newArr.push(twoLetters);
-  }
-    // check if each array has as many unique elements as original string
-  // let uniqueChars = s.filter()
-  // for (let i = 0; i < newArr.length; i++) {
-  //   if (newArr.lastIndexOf(newArr[i]) !== newArr.indexOf(newArr[i])) {
-  //     return false;
-  //   }
-  // }
-  // return true;
+
+
+  // find the index of each char and see if that index fits the same index on the other string
 };
