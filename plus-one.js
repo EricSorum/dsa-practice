8a898bf38a4ecfa63f51a2a5f10cae4341f1c39d
Eric Sorum <ejsorum@gmail.com>
2025-05-31 07:49:59 -0500

    Fixed

--- plus-one.js
+++ plus-one.js
@@ -6,10 +6,8 @@ You are given a large integer represented as an integer array digits, where each
 var plusOne = function(digits) {
   int = Number(digits.join().replaceAll(",", ""));
   int++
-  // console.log(int.toString());
-  console.log(Array.from(int.toString()));
   return Array.from(int.toString());
 };
 
 let i = ["1", "5", "9"];
-console.log("Answer: " + plusOne(i));
+console.log(plusOne(i));
